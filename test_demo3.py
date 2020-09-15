448383a4ae4a5aa09db6fd57848eb73041599ffc
Prakash <prksh037@gmail.com>
2020-09-15 23:31:23 +0530

    commit from develop branch

---
+++ test_demo3.py
@@ -0,0 +1,23 @@
+import pytest
+
+def test_firstProgram():
+    print('Hello')
+
+
+def test_secondProgram():
+    print('Hello again')
+
+
+def test_thirdProgram():
+    print('Hello again & again')
+
+def test_math_mult():
+    a = 10
+    b = 20
+    assert a * b == 200, 'Multipplication is not correct'
+
+@pytest.mark.xfail
+def test_math_div():
+    a = 10
+    b = 20
+    assert a / b == 0.5, 'Division is not correct'
